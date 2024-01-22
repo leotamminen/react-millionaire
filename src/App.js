3b108c8c8f1dae1a223951f577f73d35eb21e380
Leo Tamminen <lettam@utu.fi>
2024-01-23 00:15:50 +0200

    Quiz from components test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import { useState, useEffect } from "react";
 import { MDBRow, MDBCol, MDBListGroup, MDBBtn } from "mdb-react-ui-kit";
 import "./App.css";
+import Quiz from "./components/Quiz";
 
 function App() {
   return (
     <div className="App">
       <MDBRow>
-        <MDBCol md="9">Quiz</MDBCol>
+        <MDBCol md="9">
+          <div className="main">
+            <div style={{ height: "50%" }}>
+              <Quiz />
+            </div>
+          </div>
+        </MDBCol>
         <MDBCol md="3">Money</MDBCol>
       </MDBRow>
     </div>
