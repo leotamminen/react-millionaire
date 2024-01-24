36b9ece2f8fb06ad14402598e5a9b81adfe29fb9
Leo Tamminen <lettam@utu.fi>
2024-01-25 00:47:41 +0200

    music working, need to add other sounds

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,37 +1,73 @@
 import React, { useState, useEffect, useRef } from "react";
 import Background_music from "../assets/Background_music.mp3";
 import fiveToEight from "../assets/five-eight.mp3";
+import eightToEleven from "../assets/eight-eleven.mp3";
+import elevenToThirteen from "../assets/eleven-thirteen.mp3";
+import fourteen from "../assets/fourteen.mp3";
+import fifteen from "../assets/fifteen.mp3";
+import millionaireRave from "../assets/MillionaireRave.mp3";
 
 const Quiz = ({ questions, questionNumber, setQuestionNumber, setTimeOut }) => {
   const [question, setQuestion] = useState(null);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [className, setClassName] = useState("answer");
 
-  // Sounds
-  const backgroundMusicRef = useRef(new Audio(Background_music));
-  const fiveToEightRef = useRef(new Audio(fiveToEight));
+  // refs for various audio tracks
+  const audioRefs = {
+    backgroundMusic: useRef(new Audio(Background_music)),
+    fiveToEight: useRef(new Audio(fiveToEight)),
+    eightToEleven: useRef(new Audio(eightToEleven)),
+    elevenToThirteen: useRef(new Audio(elevenToThirteen)),
+    fourteen: useRef(new Audio(fourteen)),
+    fifteen: useRef(new Audio(fifteen)),
+    millionaireRave: useRef(new Audio(millionaireRave)),
+    // TODO: add correct/incorrect sounds, maybe lock in answer
+  };
+
+  // Better way to play multiple audios. Plays song for round and stops and resets all other audio tracks
+  const playAudio = (audioRef) => {
+    Object.values(audioRefs).forEach((ref) => {
+      if (ref.current !== audioRef.current) {
+        ref.current.pause();
+        ref.current.currentTime = 0;
+      }
+    });
+
+    // loop the playing songs
+    audioRef.current.loop = true;
+    audioRef.current.play();
+  };
 
   useEffect(() => {
-    setQuestion(questions[questionNumber - 1]);
-  }, [questions, questionNumber]);
+    const {
+      backgroundMusic,
+      fiveToEight,
+      eightToEleven,
+      elevenToThirteen,
+      fourteen,
+      fifteen,
+      millionaireRave,
+    } = audioRefs;
 
-  useEffect(() => {
-    // Set the loop attribute for continuous playback
-    backgroundMusicRef.current.loop = true;
-    fiveToEightRef.current.loop = true;
-
-    if (questionNumber < 2) {
-      backgroundMusicRef.current.play();
-      fiveToEightRef.current.pause();
-      fiveToEightRef.current.currentTime = 0;
-    }
-    if (questionNumber > 2) {
-      backgroundMusicRef.current.pause();
-      backgroundMusicRef.current.currentTime = 0;
-      fiveToEightRef.current.play();
+    // Play specific songs for question numbers. Works fine for different rounds
+    if (questionNumber < 6) {
+      playAudio(backgroundMusic);
+    } else if (questionNumber < 9) {
+      playAudio(fiveToEight);
+    } else if (questionNumber < 12) {
+      playAudio(eightToEleven);
+    } else if (questionNumber < 14) {
+      playAudio(elevenToThirteen);
+    } else if (questionNumber === 14) {
+      playAudio(fourteen);
+    } else if (questionNumber === 15) {
+      playAudio(fifteen);
+    } else if (questionNumber === 16) {
+      playAudio(millionaireRave);
     }
   }, [questionNumber]);
-  //
+
+  // Update the current question when the question number changes
   useEffect(() => {
     setQuestion(questions[questionNumber - 1]);
   }, [questions, questionNumber]);
